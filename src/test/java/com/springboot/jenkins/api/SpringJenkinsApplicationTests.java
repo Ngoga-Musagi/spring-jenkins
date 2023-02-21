e285dd2a20156d1b919872a5f3ec91777f9ab354
Ngoga-Musagi <r.alexisngoga20@gmail.com>
2023-02-21 23:01:16 +0200

    second commit

--- src/test/java/com/springboot/jenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/springboot/jenkins/api/SpringJenkinsApplicationTests.java
@@ -15,6 +15,7 @@ class SpringJenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing ...");
+		logger.info("Test case executing second logging statement ...");
 		assertEquals(true, true);
 	}
 
